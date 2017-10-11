2dbb97480040fef0c169b2799a64fe9c064bc2e3
skmbw <tongku2008@126.com>
2017-10-11 09:49:53 +0800

    注释的事务配置

--- src/main/java/com/cetiti/user/service/UserService.java
+++ src/main/java/com/cetiti/user/service/UserService.java
@@ -13,6 +13,12 @@ import org.springframework.stereotype.Service;
  * @since 2017/10/9 10:51
  */
 @Service
+// 默认的事务管理器名字是transactionManager，否则自己指定。
+// 隔离级别是DB的默认隔离级别，可设置
+// 事务传播是Required，可设置
+// 事务超时，默认是数据库的超时时间，可设置
+// 默认对运行时异常回滚，可设置
+//@Transactional
 public class UserService extends BaseService<User, String> {
 
     @Override
